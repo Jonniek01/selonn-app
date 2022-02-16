d034382e008c8fa8323300b342e7cf18709b951a
Kariuki Joseph <kariuki.joseph121@gmail.com>
2022-02-16 12:08:26 +0300

    Edited 404 page

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const NotFound = () => {
+  function back(){
+    return window.history.back();
+  }
   return (
-    <div><h1>Page not found</h1></div>
+    <div>
+      <h1>Page not found</h1>
+      <p>
+        You are requesting a resource that was not found on this server. <button onClick={back}>Take me back!</button>
+      </p>
+    </div>
   )
 }
 
